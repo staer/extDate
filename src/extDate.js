0e4141030beb5a753934c09cceb682cdeac4b871
Dan Harris <dih0658@gmail.com>
2011-01-04 00:00:23 -0500

    Added a few more switches/tests to Date.strftime()

--- src/extDate.js
+++ src/extDate.js
@@ -1,3 +1,43 @@
+var extDate = {
+    months: {
+        0: ['January', 'Jan'],
+        1: 'February',
+        2: 'March',
+        3: 'April',
+        4: 'May',
+        5: 'June',
+        6: 'July',
+        7: 'August',
+        8: 'September',
+        9: 'October',
+        10: 'November',
+        11: 'December'
+    }
+};
+
+// =====================
+// = Date.isLeapYear() =
+// =====================
+if(typeof Date.prototype.isLeapYear !== 'function') {
+    Date.prototype.isLeapYear = function() {
+        var isLeapYear = false;
+        var year = this.getFullYear();
+        if(year % 400 === 0) {
+            isLeapYear = true;
+        } else if(year % 100 === 0) {
+            isLeapYear = false;
+        } else if(year % 4 === 0) {
+            isLeapYear = true;
+        } else {
+            isLeapYear = false;
+        }
+        return isLeapYear;
+    };
+}
+
+// ===================
+// = Date.strftime() =
+// ===================
 if(typeof Date.prototype.strftime !== 'function') {
     Date.prototype.strftime = function(format) {
         var outString = "";
@@ -9,13 +49,29 @@ if(typeof Date.prototype.strftime !== 'function') {
             var directive = remainingFormat.charAt(index+1);
             outString += remainingFormat.substring(0,index);
             switch(directive) {
-                case 'Y':
+                case 'a': // Abbreviated weekday name
+                    break;
+                case 'A': // Full weekday name
+                    break;
+                case 'b': // Abbreviated month name
+                    break;
+                case 'B': // Full month name
+                    break;
+                case 'c':
+                    break;
+                case 'd':
+                    break;
+                case 'Y': // 4 digit year
                     outString += this.getFullYear();
                     break;
-                case 'y':
+                case 'y': // 2 digit year
                     var year = this.getFullYear().toString();
                     outString += year.substring(2);
                     break;
+                case '%': // % sign
+                    outString += "%";
+                    break;
+                
                 default:
                     break;
             }
@@ -28,8 +84,11 @@ if(typeof Date.prototype.strftime !== 'function') {
     };
 }
 
-if(typeof Date.strptime !== 'function') {
-    Date.strptime = function(a) { 
+// ===================
+// = String.strptime =
+// ===================
+if(typeof String.prototype.strptime !== 'function') {
+    String.prototype.strptime = function(format) { 
         return(new Date(1900,1,1)); 
     };
 }

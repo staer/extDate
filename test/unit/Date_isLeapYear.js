d4dbfcb4900f94bd604b2e299b049ab7de3c0685
Dan Harris <dih0658@gmail.com>
2011-01-04 10:22:28 -0500

    Refactored the isLeapYear() tests to be in the proper QUnit order for actual vs expected return values

--- test/unit/Date_isLeapYear.js
+++ test/unit/Date_isLeapYear.js
@@ -3,17 +3,17 @@
 // ===========================
 module("Date.isLeapYear()");
 test("Testing the year 1600 (leap year)", function() {
-	equals(true, new Date(1600, 1, 1).isLeapYear(), "");
+	equals(new Date(1600, 0, 1).isLeapYear(), true, "");
 });
 
 test("Testing the year 1700 (non leap year)", function() {
-	equals(false, new Date(1700, 1, 1).isLeapYear(), "");
+	equals(new Date(1700, 0, 1).isLeapYear(), false, "");
 });
 
 test("Testing the year 1984 (leap year)", function() {
-	equals(true, new Date(1984, 1, 1).isLeapYear(), "");
+	equals(new Date(1984, 0, 1).isLeapYear(), true, "");
 });
 
 test("Testing the year 2011 (non leap year)", function() {
-	equals(false, new Date(2011, 1, 1).isLeapYear(), "");
+	equals(new Date(2011, 0, 1).isLeapYear(), false, "");
 });
